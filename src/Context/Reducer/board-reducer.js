0e1683e3369de97ad50ea1fd303990c04a5eaf1c
Denzel <dbartolaba123@gmail.com>
2022-09-20 13:21:23 -0700

    Finishing up my logic for the board using reducer/redux

--- src/Context/Reducer/board-reducer.js
+++ src/Context/Reducer/board-reducer.js
@@ -1,4 +1,5 @@
-export defualt(state = [[4, 4, 4, 4, 4, 4], [4, 4, 4, 4, 4, 4]], action) => {
+/* eslint-disable import/no-anonymous-default-export */
+export default (state = [[4, 4, 4, 4, 4, 4], [4, 4, 4, 4, 4, 4]], action) => {
   switch (action.type) {
     case 'Update-Board':
       let { forPlayArea, row, idx } = action
@@ -12,6 +13,20 @@ export defualt(state = [[4, 4, 4, 4, 4, 4], [4, 4, 4, 4, 4, 4]], action) => {
           idx = 0;
           row = (row + 1) % 2;
         }
+        if ((i === forPlayArea - 1) && (tempBoard[row][idx] === 0 & row == activePlayer)) {
+          const oppositeRow = (row + 1) % 2;
+          tempBoard[oppositeRow][5 - idx] = 0;
+          tempBoard[row][idx] = 0;
+        }
+        else {
+          tempBoard[row][idx] = tempBoard[row][idx] + 1;
+        }
+
       }
+      return tempBoard;
+
+    default:
+      return state;
+
   }
 }
